81a752ac9060ed151dd8df7bdaed595dfa521240
taipey <ta.ta.isei1220@gmail.com>
2023-07-30 01:42:47 +0900

    弾道修正

--- Assets/Script/ShotAmmo.cs
+++ Assets/Script/ShotAmmo.cs
@@ -14,10 +14,16 @@ public class ShotAmmo : MonoBehaviour
         // マウス左クリックで発射
         if (Input.GetMouseButtonDown(0))
         {
-            GameObject ammo = Instantiate(ammoPrefab, transform.position, AKM.transform.localRotation);
+            GameObject ammo = Instantiate(ammoPrefab, transform.position, Quaternion.identity);  // 回転情報を変更
+
+            // 弾の先端の向きを銃の向いている方向に合わせる
+            ammo.transform.forward = AKM.transform.forward;
+
+            // 回転角度を調整する
+            ammo.transform.eulerAngles = new Vector3(ammo.transform.eulerAngles.x + 90, ammo.transform.eulerAngles.y, ammo.transform.eulerAngles.z);
+
             Rigidbody ammoRB = ammo.GetComponent<Rigidbody>();
             ammoRB.AddForce(transform.forward *  shotSpeed);
-
             AudioSource.PlayClipAtPoint(shotSound, Camera.main.transform.position);
         }
     }
