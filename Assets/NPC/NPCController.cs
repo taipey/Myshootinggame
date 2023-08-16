f06e9e11c4afc5421756e7ea4f5a0a5f6555aa59
taipey <ta.ta.isei1220@gmail.com>
2023-08-17 01:25:59 +0900

    更新

--- Assets/NPC/NPCController.cs
+++ Assets/NPC/NPCController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -10,56 +8,33 @@ public class NPCController : MonoBehaviour
     public float sightRange = 10f; // 視界の範囲
     public float moveSpeed = 5f; // 移動速度
     private NavMeshAgent navMeshAgent;
-    public float distance = 3.0f;
 
     private void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.speed = moveSpeed;
     }
 
     private void Update()
     {
-        if (distance > navMeshAgent.remainingDistance)
-        {
-           // navMeshAgent.ResetPath();
-            return;
-        }
-
         // プレイヤーと敵キャラクターの距離を計算
         float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+        
+        // どの位離れているかを表示
+        Debug.Log("プレイヤーまでの距離 : " + distanceToPlayer);
 
         // プレイヤーが視界に入った場合
         if (distanceToPlayer <= sightRange)
         {
             // プレイヤーの位置を追跡対象に設定
             navMeshAgent.SetDestination(player.position);
-
-            // プレイヤーの位置を敵キャラクターに向ける
-            Vector3 directionToPlayer = player.position - transform.position;
-            transform.forward = directionToPlayer.normalized;
-
-            // プレイヤーの方向を向く
-            _ = player.position - transform.position;
-            transform.forward = directionToPlayer.normalized;
-
-            // プレイヤーに向かって移動する
-            transform.position += transform.forward * moveSpeed * Time.deltaTime;
-
-            // 移動させる
-            if (!navMeshAgent.isStopped)
-            {
-                navMeshAgent.isStopped = false;
-            }
         }
         else
         {
             // プレイヤーが視界外の場合、移動を停止する
-            if (!navMeshAgent.isStopped)
-            {
-                navMeshAgent.isStopped = true;
-            }
+            navMeshAgent.ResetPath();
         }
-
+        // 不要な回転を止める
         transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, transform.eulerAngles.z);
     }
 }
--- Assets/Script/PlayerConyroller.cs
+++ Assets/Script/PlayerConyroller.cs
@@ -7,43 +7,82 @@ public class PlayerConyroller : MonoBehaviour
     public float walkSpeed;
     private float speed;
     private Vector3 movement;
-    private CharacterController controller;
+    //private CharacterController controller;
 
     private int clickCount = 0;
     public float runSpeed;
-    private Vector3 moveDir = Vector3.zero;
-    private float gravity = 3f;
+    //private Vector3 moveDir = Vector3.zero;
+    //private float gravity = 3f;
 
-    public float jumpPower;
-    public AudioClip jumpSound;
-    private AudioSource audioS;
+    //public float jumpPower;
+    //public AudioClip jumpSound;
+    //private AudioSource audioS;
+
+    private Rigidbody rb;
+
 
     void Start()
     {
-        audioS = GetComponent<AudioSource>();
+        //audioS = GetComponent<AudioSource>();
 
-        controller = GetComponent<CharacterController>();
+        //controller = GetComponent<CharacterController>();
 
         speed = walkSpeed;
+
+        TryGetComponent(out rb);
     }
 
     void Update()
+    {
+        //PlayerMove();
+
+        ProcessInput();
+    }
+    /// <summary>
+    /// 移動用のキー入力
+    /// </summary>
+    private void ProcessInput()
+    {
+        float moveH = Input.GetAxis("Horizontal");
+        float moveV = Input.GetAxis("Vertical");
+        movement = new Vector3(moveH, 0, moveV);
+    }
+
+    private void FixedUpdate()
     {
         PlayerMove();
     }
 
     void PlayerMove()
     {
-        float moveH = Input.GetAxis("Horizontal");
-        float moveV = Input.GetAxis("Vertical");
-        movement = new Vector3(moveH, 0, moveV);
+        //float moveH = Input.GetAxis("Horizontal");
+        //float moveV = Input.GetAxis("Vertical");
+        //movement = new Vector3(moveH, 0, moveV);
 
-        Vector3 desiredMove = Camera.main.transform.forward * movement.z + Camera.main.transform.right * movement.x;
-        moveDir.x = desiredMove.x * 3f;
-        moveDir.z = desiredMove.z * 3f;
-        moveDir.y -= gravity * Time.deltaTime; // 疑似重力の実装
+        //Vector3 desiredMove = Camera.main.transform.forward * movement.z + Camera.main.transform.right * movement.x;
+        //moveDir.x = desiredMove.x * 3f;
+        //moveDir.z = desiredMove.z * 3f;
+        //moveDir.y -= gravity * Time.deltaTime; // 疑似重力の実装
 
-        controller.Move(movement * Time.deltaTime * speed);
+        //controller.Move(movement * Time.deltaTime * speed);
+
+        // 64行目と処理の内容は同じですが、desiredMove 変数の処理を２つに分けます
+        // プレイヤーが傾いたまま移動すると空中に浮かぶため、その斜めの方向の力をそれぞれ除去する必要があるためです
+        // カメラの forward と right ベクトルを水平面上に投影して、移動方向を計算(単位ベクトルになっているので正規化不要)
+        Vector3 cameraForward = Camera.main.transform.forward;
+        Vector3 cameraRight = Camera.main.transform.right;
+
+        // プレイヤーが斜めに傾いている際に前後移動すると空中に浮かんでしまう
+        // その挙動を防ぐため、それぞれの Y 軸の成分を 0 にする
+        cameraForward.y = 0f;
+        cameraRight.y = 0f;
+
+        // カメラの正面方向と右方向のベクトルを使用して、プレイヤーの移動方向を計算
+        Vector3 desiredMove = cameraForward * movement.z + cameraRight * movement.x;
+
+        // Rigidbody を使って移動。また、ジャンプと落下を考慮して重力(rb.velocity.y)を反映
+        rb.velocity = desiredMove * speed + new Vector3(0, rb.velocity.y, 0);
+
 
         if (Input.GetKeyDown(KeyCode.W))
         {
@@ -58,17 +97,17 @@ public class PlayerConyroller : MonoBehaviour
             speed = walkSpeed;
         }
 
-        if (controller.isGrounded)
-        {
-            moveDir.y = 0;
-
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                moveDir.y = jumpPower;
-                audioS.clip = jumpSound;
-                audioS.Play();
-            }
-        }
+        //if (controller.isGrounded)
+        //{
+        //    moveDir.y = 0;
+        
+        //    if (Input.GetKeyDown(KeyCode.Space))
+        //    {
+        //        moveDir.y = jumpPower;
+        //        audioS.clip = jumpSound;
+        //        audioS.Play();
+        //    }
+        //}
     }
 
     void ResetCount()
